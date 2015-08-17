319614ea8edf32343ed9895728a09374f09dfa35
sysudengle <dengleshag@gmail.com>
2015-08-17 20:42:35 +0800

    preprocess fixed & train+test version1.0

--- gradient_descent.py
+++ gradient_descent.py
@@ -6,34 +6,56 @@ import math
 class Gradient_descent:
     def __init__(self):
         self.fname = {"train": "train.csv",
-                        "test": "test.csv"}
-        self.thetas = None
+                        "test": "test.csv",
+                        "result": "result.csv"}
+        self.dim = 3
+        self.thetas = [0.0 for _ in range(self.dim)]
         self.cost = None
-        self.h_func = lambda xs, thetas: sum([x * thetas[i] for i, x in enumerate(xs)])
         self.N = None
-        self.dim = None
         self.speed = 0.1
+        self.T = 5000
+
+        self.h_func = lambda xs, thetas: sum([x * thetas[i] for i, x in enumerate(xs)])
+        #self.slope_func = lambda X, Y, i, thetas: (1 / len(X)) * sum()
+        self.cost_func = lambda X, Y, hf, thetas: (1.0 / len(X)) * sum([(hf(xs, thetas) - Y[r])**2 for r, xs in enumerate(X)])
 
     def run(self):
         self.train()
         self.test()
 
     def train(self):
-        X, _ = self.readData(self.fname["train"])
-        self.gradient_descent(X)
+        X, Y = self.readData(self.fname["train"])
+        self.gradient_descent(X, Y)
 
     def test(self):
-        pass
+        X, Y = self.readData(self.fname["test"])
+        self.predict(X, Y)
 
     def readData(self, fname):
         print "Reading data...\n"
-        return ()
+        data = [[float(x) for x in xs.strip().split(',')] for xs in open(fname)]
+        X, Y = [row[:-1] for row in data], [row[-1] for row in data]
 
-    def gradient_descent(X):
-        pass
+        self.N = len(data)
 
-    def predict(self):
-        pass
+        return X, Y
+
+    def gradient_descent(self, X, Y):
+        for t in xrange(self.T):
+            for i in xrange(self.dim):
+                self.thetas[i] -= self.speed * self.slope_func(X, Y, i)
+            
+            self.cost = self.cost_func(X, Y, self.h_func, self.thetas)
+            print self.cost
+
+    # avoid closure
+    def slope_func(self, X, Y, idx):
+        return (1.0 / len(X)) * sum([xs[idx] * (self.h_func(xs, self.thetas) - Y[r]) for r, xs in enumerate(X)])
+        
+    def predict(self, X, Y):
+        with open(self.fname["result"], "w") as fd:
+            for r, xs in enumerate(X):
+                fd.write(str(self.h_func(xs, self.thetas)) + "\t==>\t" + str(Y[r]) + "\n")
 
 
 if __name__ == "__main__":
--- preprocess.py
+++ preprocess.py
@@ -15,9 +15,10 @@ if __name__ == "__main__":
             get_bounder(c, min)) for c in xrange(len(X[0]) - 1)]
 
     # normalize
+    seg = ','
     with open(sys.argv[2], "w") as output:
         for xs in X:
-            output.write(','.join([str((x - bds[c][1]) / (bds[c][0] - bds[c][1])) for c, x in enumerate(xs[:-1])] + [str(xs[-1])]) + '\n')
+            output.write('1.0' + seg + seg.join([str((x - bds[c][1]) / (bds[c][0] - bds[c][1])) for c, x in enumerate(xs[:-1])] + [str(xs[-1])]) + '\n')
     
     print '...preprocess successfully!'
 
